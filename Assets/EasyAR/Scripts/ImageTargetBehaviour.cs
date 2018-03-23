f3535e2d7cd95e9a2824f2644596c8621e5da22c
SeanZhu <zhuxiao21345@hotmail.com>
2018-03-23 09:32:26 +0800

    ADD Rotation
    Add and test rotation with idback

--- Assets/EasyAR/Scripts/ImageTargetBehaviour.cs
+++ Assets/EasyAR/Scripts/ImageTargetBehaviour.cs
@@ -5,10 +5,35 @@
 // and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
 //
 //=============================================================================================================================
+using UnityEngine;
 
 namespace EasyAR
 {
     public class ImageTargetBehaviour : ImageTargetBaseBehaviour
     {
+        protected override void Awake()
+        {
+            base.Awake();
+            TargetFound += OnTargetFound;
+            TargetLost += OnTargetLost;
+            TargetLoad += OnTargetLoad;
+            TargetUnload += OnTargetUnload;
+        }
+        void OnTargetFound(TargetAbstractBehaviour behaviour)
+        {
+            Debug.Log("Found: " + Target.Id);
+        }
+        void OnTargetLost(TargetAbstractBehaviour behaviour)
+        {
+            Debug.Log("Lost: " + Target.Id);
+        }
+        void OnTargetLoad(ImageTargetBaseBehaviour behaviour, ImageTrackerBaseBehaviour tracker, bool status)
+        {
+            Debug.Log("Load target (" + status + "): " + Target.Id + " (" + Target.Name + ") " + " -> " + tracker);
+        }
+        void OnTargetUnload(ImageTargetBaseBehaviour behaviour, ImageTrackerBaseBehaviour tracker, bool status)
+        {
+            Debug.Log("Unload target (" + status + "): " + Target.Id + " (" + Target.Name + ") " + " -> " + tracker);
+        }
     }
 }
